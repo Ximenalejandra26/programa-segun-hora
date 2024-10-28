f83226c9128c3f2d01bed8e6e44e0376413a80f1
Ximena <Ximena.aleja021@outlook.com>
2024-10-27 23:25:09 -0500

    documentamos con hora militar 6:00 mañana, 18:00 tarde

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let hora = parseInt(prompt("ingrese una hora"))
 
-if(hora<12){document.write("buenos dias")
+if(hora<12){
+    document.write("buenos dias")
+}  else if (hora<18){
+    document.write("buenas tardes")
+} else {
+    document.write("buenas noches")
 }
